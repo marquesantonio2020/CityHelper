f113b2df4609aada64d1a0fab01ae4a83896ad20
marquesantonio2020 <61158023+marquesantonio2020@users.noreply.github.com>
2021-03-20 13:00:52 +0000

    Map Inserted

--- app/src/main/java/ipvc/estg/cityhelper/fragments/IssueMapFragment.kt
+++ app/src/main/java/ipvc/estg/cityhelper/fragments/IssueMapFragment.kt
@@ -5,15 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import ipvc.estg.cityhelper.R
 
-class IssueMapFragment : Fragment() {
+private lateinit var gMap: GoogleMap
+
+
+class IssueMapFragment : Fragment(), OnMapReadyCallback {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val root = inflater.inflate(R.layout.fragment_issue_map, container, false)
+
+        //Obtaining the SupportMApFragment and get notified when the map is ready to be used
+        val frag = childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+
+        frag.getMapAsync(this)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_issue_map, container, false)
+        return root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        gMap = googleMap!!
+
+        //Add Marker in Sydney
+        val sydney = LatLng(-34.0,151.0)
+        gMap.addMarker(MarkerOptions().position(sydney).title("Sydney"))
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     companion object {
