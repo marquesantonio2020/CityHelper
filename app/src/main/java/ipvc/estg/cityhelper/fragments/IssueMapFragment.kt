6f0975943b46e447cb6e67697a98dbac738ba672
marquesantonio2020 <61158023+marquesantonio2020@users.noreply.github.com>
2021-03-20 22:49:02 +0000

    Fetching markers of issues from database

--- app/src/main/java/ipvc/estg/cityhelper/fragments/IssueMapFragment.kt
+++ app/src/main/java/ipvc/estg/cityhelper/fragments/IssueMapFragment.kt
@@ -1,10 +1,13 @@
 package ipvc.estg.cityhelper.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -13,8 +16,17 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import ipvc.estg.cityhelper.R
+import ipvc.estg.cityhelper.adapters.ReportListAdapter
+import ipvc.estg.cityhelper.api.ReportData
+import ipvc.estg.cityhelper.api.endpoints.ReportEndPoint
+import ipvc.estg.cityhelper.api.servicebuilder.ServiceBuilder
+import kotlinx.android.synthetic.main.activity_user_report_list.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 private lateinit var gMap: GoogleMap
+private lateinit var allReports: List<ReportData>
 
 
 class IssueMapFragment : Fragment(), OnMapReadyCallback {
@@ -26,8 +38,33 @@ class IssueMapFragment : Fragment(), OnMapReadyCallback {
 
         //Obtaining the SupportMApFragment and get notified when the map is ready to be used
         val frag = childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
-
         frag.getMapAsync(this)
+
+        /**Preparing request to web service - Getting all reports on database to place on de map *********/
+        val request = ServiceBuilder.buildService(ReportEndPoint::class.java)
+        val call = request.getReports()
+
+        call.enqueue(object : Callback<List<ReportData>> {
+            override fun onResponse(call: Call<List<ReportData>>, response: Response<List<ReportData>>){
+                var markerPosition: LatLng
+                if(response.isSuccessful){
+                    allReports = response.body()!!
+
+                    for(report in allReports){
+                        markerPosition = LatLng(report.report.report_location_latitude, report.report.report_location_longitude)
+
+                        gMap.addMarker(MarkerOptions().position(markerPosition).title(report.report.report_title))
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<ReportData>>, t: Throwable) {
+                Log.v("ReportHERE", "${t.message}")
+                Toast.makeText(this@IssueMapFragment.context, "${t.message}", Toast.LENGTH_LONG).show()
+            }
+        })
+
+        /********************************************/
         // Inflate the layout for this fragment
         return root
     }
@@ -36,7 +73,7 @@ class IssueMapFragment : Fragment(), OnMapReadyCallback {
         gMap = googleMap!!
 
         //Add Marker in Sydney
-        val sydney = LatLng(-34.0,151.0)
+        val sydney = LatLng(41.16418946929581, -8.628822176948432)
         gMap.addMarker(MarkerOptions().position(sydney).title("Sydney"))
         gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
