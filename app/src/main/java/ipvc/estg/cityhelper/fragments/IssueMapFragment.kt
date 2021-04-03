45402b003b89747fdb8b62fc4320624e23e6366e
marquesantonio2020 <61158023+marquesantonio2020@users.noreply.github.com>
2021-04-03 21:35:43 +0100

    Getting current user location

--- app/src/main/java/ipvc/estg/cityhelper/fragments/IssueMapFragment.kt
+++ app/src/main/java/ipvc/estg/cityhelper/fragments/IssueMapFragment.kt
@@ -11,8 +11,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -33,10 +32,17 @@ import retrofit2.Response
 
 private lateinit var gMap: GoogleMap
 private lateinit var allReports: List<ReportData>
+
+//Variables for last known location
 private lateinit var lastLocation: Location
 private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+//Variables to implement periodic updates on user's location
+private lateinit var locationCallback: LocationCallback
+private lateinit var locationRequest: LocationRequest
+
 const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+var once: Boolean = false
 class IssueMapFragment : Fragment(), OnMapReadyCallback {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,7 +55,21 @@ class IssueMapFragment : Fragment(), OnMapReadyCallback {
         frag.getMapAsync(this)
 
         //Initialize fusedLocationClient
-        //fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.context!!)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.context!!)
+        createLocationRequest()
+
+        //Callback added for periodic location updates
+        locationCallback = object: LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+                lastLocation = p0.lastLocation
+                var place = LatLng(lastLocation.latitude, lastLocation.longitude)
+                if(!once){
+                    gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12.0f))
+                    once = true
+                }
+            }
+        }
 
         /**Preparing request to web service - Getting all reports on database to place on de map *********/
         val request = ServiceBuilder.buildService(ReportEndPoint::class.java)
@@ -71,7 +91,6 @@ class IssueMapFragment : Fragment(), OnMapReadyCallback {
             }
 
             override fun onFailure(call: Call<List<ReportData>>, t: Throwable) {
-                Log.v("ReportHERE", "${t.message}")
                 Toast.makeText(this@IssueMapFragment.context, "${t.message}", Toast.LENGTH_LONG).show()
             }
         })
@@ -82,13 +101,13 @@ class IssueMapFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
-        //setUpMap()
         gMap = googleMap!!
+        setUpMap()
 //LATER CHANGE THIS SO IT STARTS ON A CITY GOTTEN FROM SHARED PREFS
         //Add Marker in Sydney
-        val porto = LatLng(41.16418946929581, -8.628822176948432)
-        gMap.addMarker(MarkerOptions().position(porto).title("Sydney"))
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(porto, 10.0f))
+        /**val porto = LatLng(41.16418946929581, -8.628822176948432)
+        gMap.addMarker(MarkerOptions().position(porto).title("Porto"))
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(porto, 10.0f))*/
     }
 
     private fun setUpMap(){
@@ -104,10 +123,36 @@ class IssueMapFragment : Fragment(), OnMapReadyCallback {
                 if(location != null){
                     lastLocation = location
                     val currentLatLng = LatLng(location.latitude, location.longitude)
-                    gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 10f))
+                    gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
                 }
             }
         }
+    }
+
+    private fun startLocationUpdates(){
+        if(ActivityCompat.checkSelfPermission(this.context!!, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this.activity!!, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    private fun createLocationRequest(){
+        locationRequest = LocationRequest()
+
+        //Specify the interval rate when the update must to be receive
+        locationRequest.interval = 6000 //ms
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
     }
 
     companion object {
